78808da6895f258d364827f02a74d4593374475a
KXiong3 <knmnx0913@gmail.com>
2021-07-17 13:16:12 -0600

    First attempt at trying to add the css. Did not go as planned and will need to revisit.

--- code.js
+++ code.js
@@ -16,101 +16,38 @@ const main = document.querySelector("main");
 
 // Your Code Here
 
-class Flower {
-  constructor() {
-    this.imageArray = ["./images/flower/flower0.png", "./images/flower/flower1.png", "./images/flower/flower2.png"];
-    this.index = 0;
-    this.imageElement = document.createElement("img");
-    this.imageElement.src = this.imageArray[this.index];
-    this.imageElement.addEventListener("click", this.clickOnImage);
-  }
-
-  clickOnImage = () =>  {
-    if (this.index < this.imageArray.length - 1) {
-      this.index++;
-      this.imageElement.src = this.imageArray[this.index];
-    }
-  };
-
-}
-
-class Charmander {
-  constructor () {
-    this.imageArray = ["./images/pokeball.png", "./images/charmander/charmander0.png","./images/charmander/charmander1.png","./images/charmander/charmander2.png"];
-    this.index = 0;
-    this.imageElement = document.createElement("img");
-    this.imageElement.src = this.imageArray[this.index];
-    this.imageElement.addEventListener("click", this.clickOnImage);
-  }
-
-  clickOnImage = () =>  {
-    let chanceToEvolve = Math.random();
-    if (chanceToEvolve < .25) {
-      if (this.index < this.imageArray.length - 1) {
-        this.index++;
-        this.imageElement.src = this.imageArray[this.index];
-      }
-    } else {
-      toggleShake(this.imageElement);
-    }
-  };
-
-}
-
-
-class Eevee {
-  constructor () {
-    this.imageArray = ["./images/pokeball.png", "./images/eevee/eevee0.png",];
-    this.finalForm = ["./images/eevee/eevee1.png","./images/eevee/eevee2.png","./images/eevee/eevee3.png","./images/eevee/eevee4.png","./images/eevee/eevee5.png","./images/eevee/eevee6.png","./images/eevee/eevee7.png","./images/eevee/eevee8.png"]
-    this.index = 0;
-    this.isFullyEvolved = false;
-    this.imageElement = document.createElement("img");
-    this.imageElement.src = this.imageArray[this.index];
-    this.imageElement.addEventListener("click", this.clickOnImage);
-  }
-
-  clickOnImage = () =>  {
-    let chanceToEvolve = Math.random();
-    if (this.isFullyEvolved) {
-      toggleShake(this.imageElement);
-    } else if (chanceToEvolve < .25) {
-      if (this.index < this.imageArray.length - 1) {
-        this.index++;
-        this.imageElement.src = this.imageArray[this.index];
-      } else {
-        let randomFinalForm = Math.floor(Math.random() * 9)
-        this.imageElement.src = this.finalForm[randomFinalForm];
-        this.isFullyEvolved = true;
-      }
-    } else {
-      toggleShake(this.imageElement);
-    }
-  };
-
-}
+let leftDiv = document.getElementById("left");
+let middleDiv = document.getElementById("middle");
+let rightDiv = document.getElementById("right");
 
 let flower1 = new Flower;
 let flower2 = new Flower;
-let flower3 = new Flower;
-let flower4 = new Flower;
-let flower5 = new Flower;
+leftDiv.append(flower1.imageElement);
+rightDiv.append(flower2.imageElement);
 
-main.append(flower1.imageElement);
-main.append(flower2.imageElement);
-main.append(flower3.imageElement);
-main.append(flower4.imageElement);
-main.append(flower5.imageElement);
+let charmander = new Charmander;
+middleDiv.append(charmander.imageElement);
 
+let eevee = new Eevee;
+middleDiv.append(eevee.imageElement);
 
-let charmander1 = new Charmander;
-let charmander2 = new Charmander;
+let abra = new Abra;
+middleDiv.append(abra.imageElement);
 
+let seviper = new Seviper;
+middleDiv.append(seviper.imageElement);
 
-main.append(charmander1.imageElement);
-main.append(charmander2.imageElement);
+let munchlax = new Munchlax;
+middleDiv.append(munchlax.imageElement)
 
-let eevee1 = new Eevee;
-let eevee2 = new Eevee;
+let blacephalon = new Blacephalon;
+middleDiv.append(blacephalon.imageElement);
 
-main.append(eevee1.imageElement);
-main.append(eevee2.imageElement);
+let blipbug = new Blipbug;
+middleDiv.append(blipbug.imageElement);
+
+let audino = new Audino;
+middleDiv.append(audino.imageElement);
+
+let blitzle = new Blitzle
+middleDiv.append(blitzle.imageElement);
